91c732b1b3f62f86caa9b24811b508a209a10c1b
farasatkhan1 <farasat.khan@jintech.com>
2025-01-14 17:57:01 +0500

    chapter 15 - Adding Authentication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,9 +1,31 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
@@ -105,6 +127,7 @@ export async function updateInvoice(
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
 export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
